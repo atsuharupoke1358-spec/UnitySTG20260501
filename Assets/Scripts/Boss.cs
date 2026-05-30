e8b284a6c5dfaa61d17732e307fc932aa4289d95
atsuharupoke1358-spec <atsuharu.poke.1358@gmail.com>
2026-05-30 22:59:44 +0900

    Update Boss.cs
    最初のコミット

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -64,7 +64,6 @@ public class Boss : MonoBehaviour
     IEnumerator ChangeStateWithDelay(BossState newState, ShotData data)
     {
         shot.enabled = false;
-
         yield return new WaitForSeconds(0.5f);
         transform.localScale =
             Vector3.one * 1.3f;
